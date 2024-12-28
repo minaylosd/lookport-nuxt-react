f79b5e3016d4eb432d10330e47ad0435acd15128
minaylosd <minaylosd@gmail.com>
2024-12-28 16:50:59 +0300

    + drei, fiber, leva libs, tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import veauryVitePlugins from 'veaury/vite/index.js';
+import veauryVitePlugins from "veaury/vite/index.js";
 
 // @ts-ignore
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [
       // @ts-ignore
       veauryVitePlugins({
-        type: 'vue',
-        isNuxt: true
-      })
+        type: "vue",
+        isNuxt: true,
+      }),
     ],
+    optimizeDeps: {
+      include: ["@react-three/fiber", "@react-three/drei"],
+    },
   },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  compatibilityDate: "2024-12-28",
 });
