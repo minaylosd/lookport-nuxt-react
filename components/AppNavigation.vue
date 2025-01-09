d13863762acac55cc485e029a0b926db6d2b3b58
minaylos-dev <minaylos.dev@gmail.com>
2025-01-09 12:55:25 +0300

    hover color and line

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -29,8 +29,9 @@ const menuItems = [
       v-for="(item, index) in menuItems"
       :key="index"
       :to="item.link"
-      class="px-6 text-[1.05rem] font-normal leading-none tracking-tighter text-[#b6b6b6] pt-[1.4rem] pb-[1.0em] font-geometria">
-      {{ item.name }}
+      class="group relative px-6 text-[1.05rem] font-normal leading-none tracking-tighter text-[#b6b6b6] hover:text-white pt-[1.4rem] pb-[1.0em] font-geometria transition-colors duration-300 ease-in-out">
+      <span>{{ item.name }}</span>
+      <div class="absolute bottom-0 left-0 flex w-[0%] h-[1px] bg-white group-hover:w-full transition-all duration-300 ease-in-out"></div>
     </NuxtLink>
   </nav>
 </template>
