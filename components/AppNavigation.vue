e7ef00f45116213f27469df134b89bafad903551
minaylos-dev <minaylos.dev@gmail.com>
2025-01-22 19:56:57 +0300

    anchor links

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -6,32 +6,28 @@ const menuItems = [
   },
   {
     name: "Our Features",
-    link: "/features",
+    link: "#features",
   },
   {
     name: "Broadcasting",
-    link: "/broadcasting",
-  },
-  {
-    name: "Previous Acts",
-    link: "/previous-acts",
+    link: "#broadcasting",
   },
   {
     name: "Contact",
-    link: "/contact",
+    link: "#contact",
   },
 ];
 </script>
 
 <template>
   <nav class="flex items-center">
-    <NuxtLink
+    <a
       v-for="(item, index) in menuItems"
       :key="index"
-      :to="item.link"
+      :href="item.link"
       class="group relative px-6 text-[1.05rem] font-normal leading-none tracking-tighter text-[#b6b6b6] hover:text-white pt-[1.4rem] pb-[1.0em] font-geometria transition-colors duration-300 ease-out">
       <span>{{ item.name }}</span>
       <div class="absolute bottom-0 left-0 flex w-[0%] h-[1px] bg-white group-hover:w-full transition-all duration-300 ease-out"></div>
-    </NuxtLink>
+    </a>
   </nav>
 </template>
