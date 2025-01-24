600baca4e95b0af5584058a30509c9e291563514
minaylos-dev <minaylos.dev@gmail.com>
2025-01-24 04:27:44 +0300

    change animation to css

--- components/Hero.vue
+++ components/Hero.vue
@@ -19,7 +19,9 @@
           Empowering promoters with Artificial Intelligence
         </p>
         <div class="flex items-center gap-4 md:flex-row">
-          <GetStartedBtn :big="true" />
+          <div class="anim-up">
+            <GetStartedBtn :big="true" />
+          </div>
           <div ref="link"
             class="anim-up whitespace-nowrap cursor-pointer text-lg font-normal leading-[13px] px-[15px] py-3 text-[#eeeeee] font-geometria text-arrow">
             Learn More</div>
@@ -29,13 +31,13 @@
       <div class="relative h-full w-full 3xl:max-w-[371px] max-w-[340px]">
         <div class="min-h-[500px] tickets">
           <!-- Верхняя картинка -->
-          <div ref="upperWrapper"
-            class="anim-up wrapper wrapper-upper [transform:perspective(600px)_rotateY(0deg)_rotateX(0deg)]">
+          <div ref="upperWrapper" class="wrapper wrapper-upper">
             <img src="/images/ticket-upper.png" alt="Interactive Image" class="interactive-image" />
             <div ref="upperGlare" class="glare"></div>
           </div>
+
           <!-- Нижняя картинка -->
-          <div ref="lowerWrapper" class="anim-up wrapper wrapper-lower">
+          <div ref="lowerWrapper" class=" wrapper wrapper-lower">
             <img src="/images/ticket-lower.png" alt="Interactive Image" class="interactive-image" />
             <div ref="lowerGlare" class="glare"></div>
           </div>
@@ -163,42 +165,6 @@ onMounted(() => {
   filter: blur(50px);
   -webkit-filter: blur(50px);
   position: relative;
-  /* Для позиционирования псевдоэлемента */
-}
-
-.black::after {
-  /* filter: blur(50px); */
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 90%;
-  height: 90%;
-  /* background: radial-gradient(ellipse 80% 60% at center, #3BAFDA 30%, transparent 55%),
-    radial-gradient(ellipse 60% 90% at bottom right, #FF40FF 35%, transparent 95%),
-    radial-gradient(ellipse 70% 50% at 50% 30%, #300dad 40%, transparent 80%); */
-  /* Градиент */
-  z-index: 1;
-  /* Псевдоэлемент должен быть поверх фона */
-  opacity: 0;
-  /* Начальная прозрачность */
-
-  /* Анимация изменения прозрачности */
-}
-
-.visible .black::after {
-  /* animation: pulse 3s infinite alternate; */
-}
-
-/* Анимация прозрачности */
-@keyframes pulse {
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 0.6;
-  }
 }
 
 .tickets {
@@ -208,6 +174,41 @@ onMounted(() => {
   align-items: center;
 }
 
+.section:not(.animated) .wrapper-lower {
+  opacity: 0;
+  transition: opacity 0.5s ease, transform 0.5s ease;
+  -webkit-transition: opacity 0.5s ease, transform 0.5s ease;
+  transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(100px) translateZ(150px);
+  -webkit-transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(100px) translateZ(150px);
+  will-change: transform;
+}
+
+
+.section:not(.animated) .wrapper-upper {
+  opacity: 0;
+  transition: opacity 0.5s ease, transform 0.5s ease;
+  -webkit-transition: opacity 0.5s ease, transform 0.5s ease;
+  transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(100px) translateZ(0);
+  -webkit-transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(100px) translateZ(0);
+  will-change: transform;
+}
+
+.animated .wrapper-lower {
+  opacity: 1;
+  transition: transform 0.2s ease-out;
+  -webkit-transition: -webkit-transform 0.2s ease-out;
+  transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(0) translateZ(150px);
+  -webkit-transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(0) translateZ(150px);
+}
+
+.animated .wrapper-upper {
+  opacity: 1;
+  transition: transform 0.2s ease-out;
+  -webkit-transition: -webkit-transform 0.2s ease-out;
+  transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(0) translateZ(0);
+  -webkit-transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateY(0) translateZ(0);
+}
+
 .wrapper {
   position: absolute;
   display: inline-block;
@@ -215,21 +216,16 @@ onMounted(() => {
   -webkit-transform-style: preserve-3d;
   perspective: 600px;
   -webkit-perspective: 600px;
-  transition: transform 0.2s ease-out;
-  -webkit-transition: -webkit-transform 0.2s ease-out;
 }
 
 .wrapper-upper {
   z-index: 2;
   left: 6px;
-  /* Верхняя картинка всегда сверху */
 }
 
 .wrapper-lower {
   z-index: 1;
-  /* Нижняя картинка находится под верхней */
   right: 6px;
-  /* transform: translateZ(150px); */
   transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateZ(150px);
   -webkit-transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateZ(150px);
 }
@@ -237,7 +233,6 @@ onMounted(() => {
 .interactive-image {
   min-width: 180px;
   width: 180px;
-  /* Размер обеих картинок одинаковый */
   height: auto;
   border-radius: 10px;
   object-fit: cover;
@@ -251,16 +246,13 @@ onMounted(() => {
 .glare {
   position: absolute;
   width: 140px;
-  /* Размер блика */
   height: 100px;
-  /* Размер блика */
   background: radial-gradient(circle, rgba(255, 255, 255, 0.93), rgba(255, 255, 255, 0.3));
   border-radius: 50%;
   pointer-events: none;
   filter: blur(20px);
   -webkit-filter: blur(20px);
   opacity: 0.6;
-  /* Блик слегка видим */
   transform: translate(-50%, -50%) scale(1);
   -webkit-transform: translate(-50%, -50%) scale(1);
   top: 50%;
@@ -276,9 +268,7 @@ onMounted(() => {
 
   .wrapper-lower {
     z-index: 1;
-    /* Нижняя картинка находится под верхней */
     right: -140px;
-    /* transform: translateZ(300px); */
     transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateZ(150px);
     -webkit-transform: perspective(600px) rotateY(0deg) rotateX(0deg) translateZ(150px);
   }
@@ -286,7 +276,6 @@ onMounted(() => {
   .interactive-image {
     min-width: 300px;
     width: 300px;
-    /* Размер обеих картинок одинаковый */
     height: auto;
     border-radius: 10px;
     object-fit: cover;
@@ -296,6 +285,5 @@ onMounted(() => {
     filter: brightness(97%);
     -webkit-filter: brightness(97%);
   }
-
 }
 </style>
